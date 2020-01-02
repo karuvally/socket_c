a1711c42262d7935b00d3aa7e147a30072db376d
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2020-01-02 23:09:30 +0530

    added file

---
+++ getaddr.c
@@ -0,0 +1,16 @@
+// include the serious stuff
+#include<stdio.h>
+#include<arpa/inet.h>
+
+// the main function
+int main()
+{
+	// essential variables
+	struct addrinfo hints, *result;
+
+	// get IP info
+	
+
+	// return zero like a gentle program
+	return 0;
+}
