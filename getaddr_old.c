85b66d0b2affe91e9d2b6c5057119767d69611ec
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2020-01-14 09:33:44 +0530

    added getaddr.c for better code

---
+++ getaddr_old.c
@@ -0,0 +1,38 @@
+// include the serious stuff
+#include<stdio.h>
+#include<stdlib.h>
+#include<arpa/inet.h>
+#include<netdb.h>
+#include<string.h>
+
+// the main function
+int main()
+{
+	// essential variables
+	struct addrinfo hints, *result;
+	char addr_str[100];
+	void result_in;
+
+	// initialize hints to zero
+	memset(&hints, 0, sizeof(hints));
+
+	// setup specification
+	hints.ai_family = PF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags |= AI_CANONNAME;
+
+	// try to get the address
+	getaddrinfo("ThinkPad-L440", NULL, &hints, &result);
+
+	// quit if result is invalid
+	if(result == NULL) {
+		printf("FATAL: Could not retrieve address");
+		exit(1);
+	}
+
+	// convert data to presentation style
+	inet_ntop(result->ai_family, result->ai_addr->sa_data, addr_str, 100);
+
+	// return zero like a gentle program
+	return 0;
+}
