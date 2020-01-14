ce65414a84ef63bbbf91ed979a31856c7587488a
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2020-01-14 09:52:33 +0530

    added variables, headers

--- getaddr.c
+++ getaddr.c
@@ -1,14 +1,23 @@
 // attempt to write a working getaddrinfo() code
-
 // include the serious stuff
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 #include<sys/types.h>
 #include<sys/socket.h>
 #include<netdb.h>
 
-
 // the main function
 int main()
 {
+	// essential variables
+	int status;
+	struct addrinfo hints;
+	struct addrinfo *server_info;
+
+	// initialize hints
+	memset(&hints, 0, sizeof(hints));
+
 	// return zero like a gentle program
 	return 0;
 }
