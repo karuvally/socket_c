ad47e98c144f225b3326398909e0e2bf3d855713
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2020-01-14 09:36:03 +0530

    added boilerplate stuff

---
+++ getaddr.c
@@ -0,0 +1,14 @@
+// attempt to write a working getaddrinfo() code
+
+// include the serious stuff
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netdb.h>
+
+
+// the main function
+int main()
+{
+	// return zero like a gentle program
+	return 0;
+}
