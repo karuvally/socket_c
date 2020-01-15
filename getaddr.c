ccaa78a0904080c78a8a9d4e4a552308fd5b03e2
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2020-01-15 06:45:32 +0530

    added error handling code

--- getaddr.c
+++ getaddr.c
@@ -19,9 +19,22 @@ int main()
 
 	// initialize hints
 	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = AF_UNSPEC;
+	hints.ai_family = AF_UNSPEC; // who knows whether its IPv4 or IPv6
 	hints.ai_socktype = SOCK_STREAM; // we like TCP :D
 	hints.ai_flags = AI_PASSIVE; // get current IP automatically
+
+	// get address related info
+	status = getaddrinfo(NULL, "22", &hints, &server_info);
+
+	// exit if something went wrong
+	if(status != 0) {
+		fprintf(
+			stderr,
+			"FATAL: getaddrinfo error %s occured\n",
+			gai_strerror(status)
+		);
+		exit(1);
+	}
 
 	// return zero like a gentle program
 	return 0;
