0bde748c3c4f992f6bce59203c4c40cf3bb7497c
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2020-01-15 06:39:49 +0530

    added code to initialize hints

--- getaddr.c
+++ getaddr.c
@@ -1,4 +1,6 @@
 // attempt to write a working getaddrinfo() code
+// Aswin Babu Karuvally (github.com/karuvally)
+
 // include the serious stuff
 #include<stdio.h>
 #include<stdlib.h>
@@ -17,6 +19,9 @@ int main()
 
 	// initialize hints
 	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM; // we like TCP :D
+	hints.ai_flags = AI_PASSIVE; // get current IP automatically
 
 	// return zero like a gentle program
 	return 0;
